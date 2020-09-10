779423ba65331952ca4bfd3ceb0370ab1c0d87a3
Lisinchyk <lisinchyk@gmail.com>
2020-09-11 00:13:01 +0300

    environment bug fixed, options cookies

--- modules/camera.js
+++ modules/camera.js
@@ -1,10 +1,10 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 export  function PerspectiveCamera_init (perspectiveCamera) {
     const fov = 45;
-    // const aspect = 1.99;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
-    const far = 10000;
+
+    let far = (get_url_param('dev')) ? 10000 : 2000;
     perspectiveCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
     return perspectiveCamera;
