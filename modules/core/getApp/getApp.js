d8c8e582ff5c74103d846cbee675291269f57749
vertalik <kolumb.vk@gmail.com>
2020-10-19 15:52:56 +0300

    add get-app section

---
+++ modules/core/getApp/getApp.js
@@ -0,0 +1,54 @@
+function creatHtmlElement(parent, elementName, elementTag, elementClass) {
+    const el = document.createElement(elementTag);
+    if (Array.isArray(elementClass)){
+        elementClass.forEach(classElement => {
+            el.classList.add(classElement);
+        });
+    }else{
+        el.style.cssText=elementClass;
+    }
+    el.textContent = elementName;
+    if (parent) {
+        parent.appendChild(el);
+    }
+    return el;
+}
+
+
+export function getApp(logoUrl, qrCode, projectName, appStoreLink,playMarketLink) {
+    const mainContainer = creatHtmlElement(document.body,'','div',['get-app__main-container']);
+    const contentContainer = creatHtmlElement(mainContainer,'','div',['get-app__content-container']);
+    const closeBtn = creatHtmlElement(contentContainer,'','img',['get-app__close-btn']);
+    closeBtn.src = './img/close-ic.svg';
+    const title = creatHtmlElement(contentContainer,'Get app','h2',['get-app__title']);
+
+    const logoWrapper = creatHtmlElement(contentContainer,'','div',['get-app__logo-wrapeer']);
+    const logoImg = creatHtmlElement(logoWrapper,'','img',['get-app__logo-img']);
+    logoImg.src = logoUrl;
+
+    const projectNameTitle = creatHtmlElement(contentContainer,`${projectName}`,'h3',['get-app__project-name']);
+
+    const qrCodeWrapper = creatHtmlElement(contentContainer,'','div',['get-app__qr-code__wrapper']);
+    const img = creatHtmlElement(qrCodeWrapper,'','img',['get-app__qr-code__img']);
+    img.src = qrCode;
+    const btnWrapper = creatHtmlElement(contentContainer,'','div',['get-app__btn-container']);
+
+    const iosLink = creatHtmlElement(btnWrapper,'','a',['get-app__link']);
+    iosLink.href=appStoreLink;
+    const iosBtn = creatHtmlElement(iosLink,'','img',['get-app__btn']);
+    iosBtn.src='./img/appstore_button.svg';
+
+    const androidLink = creatHtmlElement(btnWrapper,'','a',['get-app__link']);
+    androidLink.href=playMarketLink;
+    const androidBtn = creatHtmlElement(androidLink,'','img',['get-app__btn']);
+    androidBtn.src='./img/google_play_button.svg';
+
+    closeBtn.addEventListener('click', closeHandler);
+}
+
+function closeHandler() {
+    const container = document.querySelector('.get-app__main-container');
+    if (container){
+        container.remove();
+    }
+}
