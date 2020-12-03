fb78c305ca921f43cf8b256acef8bf59c83146d1
luckyman556 <luckyman556@gmail.com>
2020-12-03 11:43:51 +0200

    core merge fixes

--- modules/individual/flatBubble/flatBubble.js
+++ modules/individual/flatBubble/flatBubble.js
@@ -1,4 +1,5 @@
-export function flatBubbleHTML (crmData) {
+export function flatBubbleHTML (flat) {
+    let crmData = flat.userData.crm_data;
     let html;
     let textContainer = document.querySelector('.flat-bubble .text-container');
     if (textContainer.innerHTML.length == 0) {
