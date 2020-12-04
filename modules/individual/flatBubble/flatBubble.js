be969d9b7e9ea8e10b917020c17bee5235ee79a8
luckyman556 <luckyman556@gmail.com>
2020-12-04 10:10:18 +0200

    fix flat bubble

--- modules/individual/flatBubble/flatBubble.js
+++ modules/individual/flatBubble/flatBubble.js
@@ -3,11 +3,11 @@ export function flatBubbleHTML (flat) {
     let html;
     let textContainer = document.querySelector('.flat-bubble .text-container');
     if (textContainer.innerHTML.length == 0) {
-        html = `<span class="prop-num">${crmData.userData.crm_data.propNum}</span><span class="model-name">${crmData.userData.crm_data.modelName}</span>`
+        html = `<span class="prop-num">${crmData.propNum}</span><span class="model-name">${crmData.modelName}</span>`
         textContainer.innerHTML  = html;
     } else {
-        textContainer.querySelector('.prop-num').innerHTML = crmData.userData.crm_data.propNum;
-        textContainer.querySelector('.model-name').innerHTML = crmData.userData.crm_data.modelName;
+        textContainer.querySelector('.prop-num').innerHTML = crmData.propNum;
+        textContainer.querySelector('.model-name').innerHTML = crmData.modelName;
     }
     return html;
 }
